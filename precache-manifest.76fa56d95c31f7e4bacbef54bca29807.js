a1c4a39d933d959ccdc18896d52d7e3d48b7996f
dalmacijajekeva <dalmacijajekeva@gmail.com>
2020-05-02 13:24:05 +0200

    Updates

---
+++ precache-manifest.76fa56d95c31f7e4bacbef54bca29807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0297889807ced920c88dbcee7737ac7",
+    "url": "/todorovicjelena/react-ref.git/index.html"
+  },
+  {
+    "revision": "2ac01c2b690d8b4efb11",
+    "url": "/todorovicjelena/react-ref.git/static/css/main.cc652818.chunk.css"
+  },
+  {
+    "revision": "3998655102c4e704dd12",
+    "url": "/todorovicjelena/react-ref.git/static/js/2.623b7eb0.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/todorovicjelena/react-ref.git/static/js/2.623b7eb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ac01c2b690d8b4efb11",
+    "url": "/todorovicjelena/react-ref.git/static/js/main.04543c52.chunk.js"
+  },
+  {
+    "revision": "d6ce085ade015c211f70",
+    "url": "/todorovicjelena/react-ref.git/static/js/runtime-main.0bca8f18.js"
+  }
+]);
